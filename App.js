42f395767398375ce517eddd5cc0589087164459
Sushil Soni <101561727+Sushil9516@users.noreply.github.com>
2022-09-03 20:59:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import react from "react";
+// import Routings from "./Routor/Routings";
+// import School from "./Component/SuperAdminDashboard/School_registration";
+// import Student from "./Component/ADMIN_DASHBOARD/Student_registration";
+// import Teacher_Dashboard from "./Component/Teacher_Dashboard/Teacher_Dashboard";
+import Calender from "./Component/Teacher_Dashboard/Calender/Calender";
+// import "./Component/Teacher_Dashboard/Calender/Calender.css";
+import Practice from "./Practice";
+
+function App() {
+  return (
+    <div>
+      {/* <Routings /> */}
+      {/* <Student /> */}
+      {/* <Teacher_Dashboard /> */}
+      <Calender/>
+      {/* <h1>Hello</h1> */}
+      {/* <Practice/> */}
+    </div>
+  );
+}
+
+export default App;
