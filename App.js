5655dd51c7fbd7103d1b182686fc06ded79099de
Sushil Soni <101561727+Sushil9516@users.noreply.github.com>
2022-09-03 21:00:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,20 +2,11 @@ import react from "react";
 // import Routings from "./Routor/Routings";
 // import School from "./Component/SuperAdminDashboard/School_registration";
 // import Student from "./Component/ADMIN_DASHBOARD/Student_registration";
-// import Teacher_Dashboard from "./Component/Teacher_Dashboard/Teacher_Dashboard";
-import Calender from "./Component/Teacher_Dashboard/Calender/Calender";
-// import "./Component/Teacher_Dashboard/Calender/Calender.css";
-import Practice from "./Practice";
-
+import Test from "./Component/Teacher_Dashboard/Calender/Calender";
 function App() {
   return (
     <div>
-      {/* <Routings /> */}
-      {/* <Student /> */}
-      {/* <Teacher_Dashboard /> */}
-      <Calender/>
-      {/* <h1>Hello</h1> */}
-      {/* <Practice/> */}
+      <Test/>
     </div>
   );
 }
